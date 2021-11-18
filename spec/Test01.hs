b03054105d6555e0ef3fe744e55ceeb2b409f136
rvLMdE7 <75222896+rvLMdE7@users.noreply.github.com>
2021-11-18 00:57:02 +0000

    fix tests for day 01 & finish part 2

--- spec/Test01.hs
+++ spec/Test01.hs
@@ -8,9 +8,9 @@ import Test.Tasty qualified as Tasty
 import Test.Tasty.HUnit ((@?=))
 import Test.Tasty.HUnit qualified as Tasty.HUnit
 import Text.Megaparsec qualified as Par
+import Flow ((.>))
 
 import Day01 qualified
-import Flow ((.>))
 
 
 main :: IO ()
@@ -45,8 +45,16 @@ logicTests :: TestTree
 logicTests = Tasty.testGroup "Logic tests"
     [ Tasty.HUnit.testCase "partial sums" $
         Day01.partialSums @Int [1, -2, 3, 1] @?= [0, 1, -1, 2, 3]
-    , Tasty.HUnit.testCase "loop 1" $
-        Day01.firstRepeat (makeLoop [1, -2, 3, 1]) @?= Just @Int 2
+    , Tasty.HUnit.testCase "main loop" $
+        Day01.firstRepeat (makeLoop [1, -2, 3, 1]) @?= Just 2
+    , Tasty.HUnit.testCase "extra loop 1" $
+        Day01.firstRepeat (makeLoop [1, -1]) @?= Just 0
+    , Tasty.HUnit.testCase "extra loop 2" $
+        Day01.firstRepeat (makeLoop [3, 3, 4, -2, -4]) @?= Just 10
+    , Tasty.HUnit.testCase "extra loop 3" $
+        Day01.firstRepeat (makeLoop [-6, 3, 8, 5, -6]) @?= Just 5
+    , Tasty.HUnit.testCase "extra loop 4" $
+        Day01.firstRepeat (makeLoop [7, 7, -2, -7, -4]) @?= Just 14
     ]
   where
-    makeLoop = Day01.partialSums .> cycle
+    makeLoop = cycle .> Day01.partialSums @Int
