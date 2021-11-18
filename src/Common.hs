0a4663f9b06d62c3b77c8a3d13ab5d2a4f111a5c
rvLMdE7 <75222896+rvLMdE7@users.noreply.github.com>
2021-11-18 00:24:14 +0000

    set up input files

--- src/Common.hs
+++ src/Common.hs
@@ -1,16 +1,21 @@
 module Common where
 
+import Control.Monad ((>=>))
 import Data.Bifunctor (first)
 import Data.ByteString qualified as Byt
 import Data.Text (Text)
 import Data.Text.Encoding qualified as Text.Enc
 import Data.Void (Void)
 import Flow ((.>))
+import Paths_adventofcode2018 (getDataFileName)
 import Text.Megaparsec qualified as Par
 
 
 readFileUtf8 :: FilePath -> IO Text
 readFileUtf8 path = Text.Enc.decodeUtf8 <$> Byt.readFile path
+
+readInputFileUtf8 :: FilePath -> IO Text
+readInputFileUtf8 = getDataFileName >=> readFileUtf8
 
 type Parser a = Par.Parsec Void Text a
 
